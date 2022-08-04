d393f1eb6696f78c40d9d8339d4cf7d6fb51ca7f
Jan Egil Ring <jan.egil.ring@outlook.com>
2022-08-04 10:00:22 +0000

    Added pim function
    Signed-off-by: Jan Egil Ring <jan.egil.ring@outlook.com>

--- .devcontainer/powershell/azure_pim.ps1
+++ .devcontainer/powershell/azure_pim.ps1
@@ -1,16 +1,20 @@
 Install-Module JaZ.PIM
 Install-Module Microsoft.Graph.DeviceManagement.Enrolment
 
-Import-Module JaZ.PIM
-Update-Module JaZ.PIM
+function pim {
 
+Import-Module JaZ.PIM
 Select-MgProfile 'Beta'
 
-gcm -mod JaZ.PIM
+Connect-MgGraph -Scopes 'RoleEligibilitySchedule.ReadWrite.Directory','RoleManagement.ReadWrite.Directory' -ContextScope Process -UseDeviceAuthentication
+Enable-JAzADRole 'Global Administrator (65e16447-3c6c-42ba-adec-7bb113c64d7b)' -Justification 'Daily elevation' -Hours 8
 
-Get-JAzADRole
-
+Connect-AzAccount -Tenant b716bd50-85d2-417b-8540-2a4d8d97f738 -UseDeviceAuthentication
 Enable-JAzRole 'Owner -> IT (b33cf45a-bfa5-4a75-8d06-05f95ed25536)' -Justification 'Daily elevation' -Hours 8
+
+}
+
+pim
 
 # Modules that must be imported into the global environment prior to importing this module
 RequiredModules  = @('Az.Resources', 'Microsoft.Graph.DeviceManagement.Enrolment')
@@ -22,7 +26,7 @@ Enable-JAzRole <tab or shift-enter>
 Disable-JAzRole <tab or shift-enter>
 
 #Connect-MgGraph with an appropriate scope like 'RoleEligibilitySchedule.ReadWrite.Directory' or 'RoleManagement.ReadWrite.Directory'
-Connect-MgGraph -Scopes 'RoleEligibilitySchedule.ReadWrite.Directory','RoleManagement.ReadWrite.Directory' -ContextScope Process -UseDeviceAuthentication
+
 
 Get-JAzADRole
 
@@ -30,3 +34,4 @@ Enable-JAzADRole <tab or shift-enter>
 Disable-JAzADRole <tab or shift-enter>
 
 Enable-JAzADRole
+
